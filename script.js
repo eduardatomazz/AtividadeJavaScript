12826185185a4f8425179782078b742febb3fd63
Eduarda <eduarda_tomaz@estudante.sesisenai.org.br>
2025-03-18 10:07:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//atividade 1
+let numero = 1
+function calculo(numero){
+    if (numero % 2 === 0){
+        console.log("É um número par");
+    } else {
+        console.log("É um número ímpar");
+    }
+}
+calculo(numero)
+
+//atividade 2
+let numeroum = 2
+let numerodois = 4
+function calcular(numeroum, numerodois){
+   console.log(numeroum + numerodois)
+   console.log(numeroum - numerodois)
+   console.log(numeroum * numerodois)
+   console.log(numeroum / numerodois)
+}
+calcular(numeroum, numerodois)
+
+//atividade 3
+let count = 10;
+while(count >= 1){
+    console.log(count);
+    count--;
+}
+
+//atividade 4
+function interverTexto("Casa"){
+    return "Casa";
+}
+interverTexto("Casa");
+
+
+
